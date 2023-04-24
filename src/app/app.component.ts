aeb7514358e8e22b82ac084bd14c0c9d5235887e
Babu@git <jb2467382@gmail.com>
2023-04-24 21:48:18 +0530

    sdafsdhj

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularassignments2';
-  arr=[21,23,44,34,54,64,12,10]
+  arr=["https://i0.wp.com/iwbond.org/wp-content/uploads/2019/04/White-Lion.jpg?fit=1600%2C901&ssl=1","https://wildlifeinformer.com/wp-content/uploads/2020/12/gorilla.jpg","https://th.bing.com/th/id/R.c0e91ddf4dc6dce51f86ca48a417ab3f?rik=wTz2%2ber4W8tbpQ&riu=http%3a%2f%2fwww.pixelstalk.net%2fwp-content%2fuploads%2f2016%2f07%2fCute-Baby-Animal-Image-Free-Download.jpg&ehk=jq9w7hdot37D5MFlWCA8aqoqnSewDSHU5KaniBfHxT0%3d&risl=&pid=ImgRaw&r=0","https://149367219.v2.pressablecdn.com/wp-content/uploads/2016/12/Animals-1.jpg","https://i.pinimg.com/736x/af/26/ac/af26acae057477774a4e2326c2234e22.jpg"]
 }
