bf3ff8e4d79cb0775cf6bd677c02ac51de2c0ee4
Babu@git <jb2467382@gmail.com>
2023-04-24 22:14:02 +0530

    sadfsdgf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularassignments2';
-  arr=[12,34,44,33,57,89,79]
+arr=['https://i.pinimg.com/736x/af/26/ac/af26acae057477774a4e2326c2234e22.jpg','https://th.bing.com/th/id/OIP.WLiI49FndyY2niPN48dFrwHaFj?pid=ImgDet&rs=1','https://www.rd.com/wp-content/uploads/sites/2/2016/12/01_Cottontails-10-days-copy.jpg']
 }
