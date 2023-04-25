02e484eede32be02f4141465b0ba75ad29740f71
Babu@git <jb2467382@gmail.com>
2023-04-25 12:40:31 +0530

    dfsiof

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularassignments2';
-  arr=[12,34,44,33,57,89,79]
+  tasks = [
+    {
+      title: 'Task 1',
+      description: 'Do something',
+      status: 'Incomplete',
+    },
+    {
+      title: 'Task 2',
+      description: 'Do something else',
+      status: 'Complete',
+    },
+    {
+      title: 'Task 3',
+      description: 'Do another thing',
+      status: 'Incomplete',
+    }, {
+      title: 'Task 4',
+      description: 'Do another thing',
+      status: 'Incomplete',
+    }, {
+      title: 'Task 5',
+      description: 'Do another thing',
+      status: 'complete',
+    }, {
+      title: 'Task 6',
+      description: 'Do another thing',
+      status: 'Incomplete',
+    },
+  ];  
 }
