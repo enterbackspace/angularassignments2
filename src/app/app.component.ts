213f9c398f761b488717042480764d1a0e922fbc
Babu@git <jb2467382@gmail.com>
2023-04-25 19:20:20 +0530

    sadfbn

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularassignments2';
-  arr=[12,34,44,33,57,89,79]
+  onClick(i:any){
+    console.log(i);
+
+  }
 }
