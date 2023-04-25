bfad67b5841dfc2843922348dd109e00a72d4655
Babu@git <jb2467382@gmail.com>
2023-04-25 12:06:49 +0530

    LJSCKVl

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularassignments2';
-  arr=[12,34,44,33,57,89,79]
+    ob:any = [{
+    firstname:"praveen",
+    lastname:"gubbala",
+    gender:"male",
+    age:40
+  }]
 }
