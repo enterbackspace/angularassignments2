74718767e337077ff0fdae660d02d9d84c26abf2
Babu@git <jb2467382@gmail.com>
2023-04-25 14:51:27 +0530

    ifdsego

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularassignments2';
-  arr=[12,34,44,33,57,89,79]
+  border="2px solid black";
+  width="2px solid red";
 }
