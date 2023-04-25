12d8b3fa19ae9470299971fecce7f7eec98e8e8a
Babu@git <jb2467382@gmail.com>
2023-04-25 16:54:36 +0530

    aDDDFG

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularassignments2';
-  arr=[12,34,44,33,57,89,79]
+   
+  selectedValue: string = '';
+
+  displaySelectedValue(event: any) {
+    this.selectedValue = event.target.value;
+  }
 }
